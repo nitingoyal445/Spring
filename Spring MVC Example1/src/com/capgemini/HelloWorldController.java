1b6f9fa5459a011ff413b4981e5d27e1c3bc13c0
nitingoyal445 <35740188+nitingoyal445@users.noreply.github.com>
2019-03-04 14:42:05 +0530

    Add files via upload

---
+++ Spring MVC Example1/src/com/capgemini/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.capgemini;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/hello")
+	public ModelAndView helloWorld(){
+		
+		String message = "Hello Nitin";
+		return new ModelAndView("hellopage","message",message);
+	}
+	
+
+
+}
